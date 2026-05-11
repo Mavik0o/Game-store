c5a3bdf514d2012bf5cf66a5e95d6366428a20c4
Łukasz Dziubiela <s33646@pjwstk.edu.pl>
2026-05-11 16:21:51 +0200

    Usunalem mappera, dodalem Controller dla admina i chyba security config jakis teraz zrobie

--- src/main/java/org/example/gamestore/controllers/AdminGameController.java
+++ src/main/java/org/example/gamestore/controllers/AdminGameController.java
@@ -1,5 +1,37 @@
 package org.example.gamestore.controllers;
 
+import jakarta.validation.Valid;
+import org.example.gamestore.dto.GameRequestDto;
+import org.example.gamestore.dto.GameResponseDto;
+import org.example.gamestore.services.GameService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/admin/games")
 public class AdminGameController {
 
+    private final GameService gameService;
+
+    public AdminGameController(GameService gameService) {
+        this.gameService = gameService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public GameResponseDto create(@RequestBody @Valid GameRequestDto requestDto) {
+        return gameService.create(requestDto);
+    }
+
+    @PutMapping("/{id}")
+    public GameResponseDto update(@PathVariable Long id,
+                                  @RequestBody @Valid GameRequestDto requestDto) {
+        return gameService.update(id, requestDto);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id) {
+        gameService.delete(id);
+    }
 }
